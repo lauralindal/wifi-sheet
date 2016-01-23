a15342e41cc98ad2f9dceafab73af9bca4a02d4e
Laura Laugwitz <s0544049@htw-berlin.de>
2016-01-23 22:07:14 +0100

    Refactor

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 document.addEventListener('DOMContentLoaded', init);
 
+function generateString() {
+  var ssid = document.getElementById("ssid").value;
+  var password = document.getElementById("password").value;
+  var encryption = document.getElementById("encryption").value;
+  var hidden = document.getElementById("hidden").checked;
+
+  return "WIFI:S:" + ssid + ";T:" + encryption + ";P:" + password + ";H:" + hidden + ";;"
+}
+
+function updateQRCode() {
+  var text = generateString();
+  var element = document.getElementById("qrcode");
+  var bodyElement = document.body;
+  if(element.lastChild)
+    element.replaceChild(showQRCode(text), element.lastChild);
+  else
+    element.appendChild(showQRCode(text));
+  populateStorage();
+}
+
+function populateStorage() {
+  localStorage.setItem('ssid', document.getElementById('ssid').value);
+  localStorage.setItem('encryption', document.getElementById('encryption').value);
+  localStorage.setItem('hidden', document.getElementById('hidden').checked);
+}
+
+function setFromStorage() {
+  document.getElementById('ssid').value = localStorage.getItem('ssid');
+  document.getElementById('encryption').value = localStorage.getItem('encryption');
+  document.getElementById('hidden').checked = localStorage.getItem('hidden');
+}
 
 function init() {
-
-   var ssid = document.querySelector('ssid');
-   var password = document.querySelector('password');
-   var encryption = document.querySelector('encryption');
-   var hidden = document.querySelector('hidden');
-
-
-   ssid.addEventListener('change', function() {
-
-      var betrag = input.value;
-      var prozent = select.value;
-      var summe = +betrag + betrag * prozent;
-
-      output.value = summe + ' €';
-
-   });
-
+  if(!!localStorage.getItem('ssid')) {
+    setFromStorage();
+    updateQRCode();
+  } else {
+    updateQRCode();
+  }
 }
